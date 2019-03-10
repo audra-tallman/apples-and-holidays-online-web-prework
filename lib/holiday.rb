160d0546360ee2adbc9c57905923822ff56fb395
Audra Tallman <audra.tallman@hotmail.com>
2019-03-10 22:54:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,7 +21,6 @@ holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
 holiday_hash[:winter].each do |holiday, supplies|
   supplies.flatten
   end
