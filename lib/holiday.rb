d5ee9f279e76ba9598544aadec84f41059029a7c
Audra Tallman <audra.tallman@hotmail.com>
2019-03-10 22:41:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-holiday_hash.each do |season, holiday_name|
+holiday_hash.each do |season, holidays|
   
 
 end
