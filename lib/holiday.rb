412189132fbb4561ee5d278b655b22f75aafa207
Audra Tallman <audra.tallman@hotmail.com>
2019-03-10 22:47:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,6 +73,10 @@ def all_holidays_with_bbq(holiday_hash)
   holidays_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
   if supplies.include? "BBQ" 
+    puts supplies
+    
+   end
+  end 
 end
 
 
