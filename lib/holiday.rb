686bf73f47cc39dc3eaeb00d1fa8db159bef17a3
Audra Tallman <audra.tallman@hotmail.com>
2019-03-10 22:37:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,7 +39,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-holiday_hash[:fall] == {:thanksgiving=>["Turkey"]}
+holiday_hash[season] == {:thanksgiving=>["Turkey"]}
 holiday_hash
 end
 
